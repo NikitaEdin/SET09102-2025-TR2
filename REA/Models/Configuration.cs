25506bb65e4773a6ce44a17a76fccf37c1947a91
Blade-iii <54221403+Blade-iii@users.noreply.github.com>
2025-04-06 15:05:33 +0100

    Created models
    Models to be used for the database.

---
+++ REA/Models/Configuration.cs
@@ -0,0 +1,14 @@
+﻿
+namespace REA.Models
+{
+    public class Configuration
+    {
+        public int configID { get; set; }
+        public int sensorID { get; set; }
+        public float minMeasurement { get; set; }
+        public float maxMeasurement { get; set; }
+        public string type { get; set; }
+        public float firmware { get; set; }
+
+    }
+}
