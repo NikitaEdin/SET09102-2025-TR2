99f8a3e1ba4b5b815588e8a9a8cb47d67bccd76b
NikitaEdin <nikita.lanetsky.uk@gmail.com>
2025-04-08 14:54:34 +0100

    Add comments and util methods

--- REA/Models/Metadata.cs
+++ REA/Models/Metadata.cs
@@ -1,6 +1,11 @@
-﻿using SQLite;
+﻿using REA.DB;
+using SQLite;
 
 namespace REA.Models {
+    /// <summary>
+    /// Metadata model holding record data of metadata attributes
+    /// Author: Nikita Lanetksy.
+    /// </summary>
     public class Metadata {
         [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
@@ -28,5 +33,34 @@ namespace REA.Models {
         public string reference_url { get; set; }
 
         public int? Sensor_ID { get; set; }
+
+
+        /// <summary>
+        /// Retrieve all records from database of this type
+        /// </summary>
+        /// <returns>Return list of MetaData from DB</returns>
+        public static async Task<List<Metadata>> GetAllMetadataAsync() {
+            var items = await SQLiteDatabaseService.Instance.GetItemsAsync<Metadata>();
+            return items?.ToList() ?? new List<Metadata>();
+        }
+
+        /// <summary>
+        /// Override the base ToString to print all object attributes
+        /// </summary>
+        /// <returns>String containing all object attributes</returns>
+        public override string ToString() {
+            return $"ID: {ID}, " +
+                   $"Category: {category}, " +
+                   $"Quantity: {quantity}, " +
+                   $"Symbol: {symbol}, " +
+                   $"Unit: {unit}, " +
+                   $"Unit Description: {(string.IsNullOrEmpty(unit_drescription) ? "null" : unit_drescription)}, " +
+                   $"Measurement Frequency: {(string.IsNullOrEmpty(measurement_freq) ? "null" : measurement_freq)}, " +
+                   $"Safe Level: {(safe_level.HasValue ? safe_level.Value.ToString("F2") : "null")}, " +
+                   $"Sensor: {(string.IsNullOrEmpty(Sensor) ? "null" : Sensor)}, " +
+                   $"Reference URL: {(string.IsNullOrEmpty(reference_url) ? "null" : reference_url)}, " +
+                   $"Sensor ID: {(Sensor_ID.HasValue ? Sensor_ID.Value.ToString() : "null")}";
+        }
+
     }
 }
