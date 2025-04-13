2d26e94e5244b09e733b6b42ba827e21932a5387
Blade-iii <54221403+Blade-iii@users.noreply.github.com>
2025-04-13 19:35:59 +0100

    Seperated the collections

--- REA/ViewModels/UpdateSensorViewModel.cs
+++ REA/ViewModels/UpdateSensorViewModel.cs
@@ -10,30 +10,46 @@ using REA.Models;
 using System.Diagnostics;
 using Microsoft.Maui.Controls;
 using REA.DB;
+using static Microsoft.Maui.ApplicationModel.Permissions;
 
 namespace REA.ViewModels
 {
     public partial class UpdateSensorViewModel : ObservableObject
     {
+        // AIR
         [ObservableProperty]
         private ObservableCollection<Configuration> airSensorType;
-
         [ObservableProperty]
         private ObservableCollection<Configuration> nitrogenDioxide;
-
         [ObservableProperty]
         private ObservableCollection<Configuration> sulphurDioxide;
-
         [ObservableProperty]
         private ObservableCollection<Configuration> particulateMatter;
 
-
-
+        // WATER
         [ObservableProperty]
         private ObservableCollection<Configuration> waterSensorType;
-       
+        [ObservableProperty]
+        private ObservableCollection<Configuration> nitrate;
+        [ObservableProperty]
+        private ObservableCollection<Configuration> phosphate;
+        [ObservableProperty]
+        private ObservableCollection<Configuration> escherichiaColi;
+        [ObservableProperty]
+        private ObservableCollection<Configuration> intestinalEnterococci;
+
+
+        // WEATHER
         [ObservableProperty]
         private ObservableCollection<Configuration> weatherSensorType;
+        [ObservableProperty]
+        private ObservableCollection<Configuration> airTemperature;
+        [ObservableProperty]
+        private ObservableCollection<Configuration> humidity;
+        [ObservableProperty]
+        private ObservableCollection<Configuration> windSpeed;
+        [ObservableProperty]
+        private ObservableCollection<Configuration> windDirection;
 
         // Drop down of the different sensor types
         [ObservableProperty]
@@ -49,11 +65,18 @@ namespace REA.ViewModels
         [ObservableProperty]
         private string sensorFirmware;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public UpdateSensorViewModel()
         {
             LoadConfigs();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public async Task LoadConfigs()
         {
             // Call the factory
@@ -70,8 +93,24 @@ namespace REA.ViewModels
                 SulphurDioxide = new ObservableCollection<Configuration>(configs.Where(c => c.Type == "Sulphur dioxide"));
                 ParticulateMatter = new ObservableCollection<Configuration>(configs.Where(c => c.Type == "Particulate matter"));
 
+                // Water
                 WaterSensorType = new ObservableCollection<Configuration>(configs.Where(c => c.Type == "Nitrate" || c.Type == "Phosphate" || c.Type == "Escherichia coli" || c.Type == "Intestinal enterococci"));
+
+                Nitrate = new ObservableCollection<Configuration>(configs.Where(c => c.Type == "Nitrate"));
+                Phosphate = new ObservableCollection<Configuration>(configs.Where(c => c.Type == "Phosphate"));
+                EscherichiaColi = new ObservableCollection<Configuration>(configs.Where(c => c.Type == "Escherichia coli"));
+                IntestinalEnterococci = new ObservableCollection<Configuration>(configs.Where(c => c.Type == "Intestinal enterococci"));
+
+
+                // Weather
                 WeatherSensorType = new ObservableCollection<Configuration>(configs.Where(c => c.Type == "Air Temperature" || c.Type == "Humidity" || c.Type == "Wind speed" || c.Type == "Wind Direction"));
+
+                AirTemperature = new ObservableCollection<Configuration>(configs.Where(c => c.Type == "Air Temperature"));
+                Humidity = new ObservableCollection<Configuration>(configs.Where(c => c.Type == "Humidity"));
+                WindSpeed = new ObservableCollection<Configuration>(configs.Where(c => c.Type == "Wind speed"));
+                WindDirection = new ObservableCollection<Configuration>(configs.Where(c => c.Type == "Wind Direction"));
+
+
                 Debug.WriteLine(configs);
 
             }
@@ -80,7 +119,7 @@ namespace REA.ViewModels
                 Debug.WriteLine("UNABLE TO POPULATE ");
             }
 
-            SensorTypes = new ObservableCollection<string> { "Air", "Nitrogen dioxide","Sulphur dioxide", "Particulate matter", "Water", "Weather" };
+            SensorTypes = new ObservableCollection<string> { "Nitrogen dioxide","Sulphur dioxide", "Particulate matter", "Nitrate", "Phosphate" , "Escherichia coli" , "Intestinal enterococci", "Air Temperature", "Humidity", "Wind speed", "Wind Direction" };
 
         }
 
