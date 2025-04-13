c3f7a285936308d5564a36cf30e7aa95988970d2
Blade-iii <54221403+Blade-iii@users.noreply.github.com>
2025-04-13 15:46:31 +0100

    Added Database integration
    Next step is to update the ui and start doing CRUD operations

--- REA/ViewModels/UpdateSensorViewModel.cs
+++ REA/ViewModels/UpdateSensorViewModel.cs
@@ -1,19 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+using REA.Utils;
+using REA.Models;
+using System.Diagnostics;
+using Microsoft.Maui.Controls;
 
 namespace REA.ViewModels
 {
     public partial class UpdateSensorViewModel : ObservableObject
     {
-      public UpdateSensorViewModel()
-        {
+        [ObservableProperty]
+        private ObservableCollection<Configuration> waterSensorType;
+        [ObservableProperty]
+        private ObservableCollection<Configuration> airSensorType;
+        [ObservableProperty]
+        private ObservableCollection<Configuration> weatherSensorType;
 
+        [ObservableProperty]
+        private ObservableCollection<string> sensorTypes;
+
+        public UpdateSensorViewModel()
+        {
+            LoadConfigs();
         }
 
+        public async Task LoadConfigs()
+        {
+            ConfigFactory factory = await ConfigFactory.CreateAsync();
+
+            ObservableCollection<Configuration> configs = factory.GetConfigurations();
+
+            if (configs != null)
+            {
+                AirSensorType = new ObservableCollection<Configuration>(configs.Where(c => c.Type == "Nitrogen dioxide" || c.Type == "Sulphur dioxide" || c.Type == "Particulate matter"));
+                WaterSensorType = new ObservableCollection<Configuration>(configs.Where(c => c.Type == "Nitrate" || c.Type == "Phosphate" || c.Type == "Escherichia coli" || c.Type == "Intestinal enterococci"));
+                WeatherSensorType = new ObservableCollection<Configuration>(configs.Where(c => c.Type == "Air Temperature" || c.Type == "Humidity" || c.Type == "Wind speed" || c.Type == "Wind Direction"));
+                Debug.WriteLine(configs);
+
+            }
+            else
+            {
+                Debug.WriteLine("UNABLE TO POPULATE ");
+            }
+
+            SensorTypes = new ObservableCollection<string> { "Air", "Water", "Weather" };
+
+        }
         
     }
 }
