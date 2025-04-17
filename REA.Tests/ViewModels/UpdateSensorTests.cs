d7ec6c2947195229292c526701aea7f477a1fd06
Blade-iii <54221403+Blade-iii@users.noreply.github.com>
2025-04-17 14:22:21 +0100

    Create UpdateSensorTests.cs
    Create the test and import libraries needed for testing

---
+++ REA.Tests/ViewModels/UpdateSensorTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using REA.DB;
+using REA.Models;
+using REA.ViewModels;
+using REA.Tests.Services;
+
+namespace REA.Tests.ViewModels
+{
+    /// <summary>
+    /// Unit tests for the UpdateSensorViewModel
+    /// Author: Thomas Smith
+    /// </summary>
+    public class UpdateSensorTests
+    {
+        private readonly IDatabaseService fakeDb;
+        private UpdateSensorViewModel viewModel;
+
+        public UpdateSensorTests() 
+        { 
+            viewModel = new UpdateSensorViewModel();
+            fakeDb = new FakeDatabaseService();
+        }
+    }
+}
